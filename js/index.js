84fb47c3df422bd50b8462d00bbfd6bb7ccdb33e
Vladimir <indigosunset00@gmail.com>
2023-03-04 14:47:21 +0600

    pushing

--- js/index.js
+++ js/index.js
@@ -1,64 +1,107 @@
-const buttonToggleLike = document.querySelector('.song-like');
-
-function buttonToggle(parent){
+function buttonToggle(parent) {
     const buttonActive = parent.querySelector('.song-like__icon--fill');
     const buttonNotActive = parent.querySelector('.song-like__icon');
     buttonNotActive.classList.toggle("song-like__icon--hide");
     buttonActive.classList.toggle("song-like__icon--hide");
 }
 
-const allItems = document.querySelector(".song-grid");
-allItems.addEventListener("click", (e) =>{
-    const targetIcon = e.target.closest(".song-like__icon");
-    if (targetIcon) {
-        const parent = targetIcon.closest(".song-item");
-        buttonToggle(parent);
-    }
-});
+const allItems = document.querySelector("body");
+if (document.querySelector('body')) {
+    allItems.addEventListener("click", (e) => {
 
-const buttonToggleStart = document.querySelector('.song-start');
+        const targetIcon = e.target.closest(".song-like__icon");
+        if (targetIcon) {
+            const parent = targetIcon.closest(".song-item");
+            buttonToggle(parent);
+        }
 
-function startToggle(parent){
-    const startActive = parent.querySelector('.song-start__icon--stop');
-    const startNotActive = parent.querySelector('.song-start__icon');
-     startNotActive.classList.toggle("song-start__icon--hide");
-     startActive.classList.toggle("song-start__icon--hide");
+    });
+}
+;
+
+function startToggle(parent) {
+    const startActive = parent.querySelector('.song-start__icon');
+    const startNotActive = parent.querySelector('.song-start__icon--stop');
+    startNotActive.classList.toggle("song-start__icon--hide");
+    startActive.classList.toggle("song-start__icon--hide");
 }
 
-const allItemsStart = document.querySelector(".song-grid");
-allItemsStart.addEventListener("click", (e) =>{
-    const targetStart = e.target.closest(".song-start__icon");
-    if (targetStart) {
-        const parent = targetStart.closest(".song-item");
-        startToggle(parent);
-    }
-});
- const inputSwitch = document.querySelector('.form-check-input');
- const bodyTheme =document.querySelector('body')
- inputSwitch.addEventListener("click" , function (){
-     bodyTheme.classList.toggle('dark-theme')
- })
-
-const hoverContainer = document.querySelector('.player-wrapper');
-const playerDropdown = document.querySelector('.song-dropdown');
-function hoverToggle(){
-    playerDropdown.classList.toggle('song-dropdown--show');
+const allItemsStart = document.querySelector("body");
+if (document.querySelector('body')) {
+    allItemsStart.addEventListener("click", (e) => {
+        const targetStart = e.target.closest(".song-start__icon");
+        if (targetStart) {
+            const parent = targetStart.closest(".song-item");
+            startToggle(parent);
+        }
+    });
 };
-hoverContainer.addEventListener("click", hoverToggle);
+function voteToggle(parent) {
+    const voteActive = parent.querySelector('.song-vote__icon');
+    const voteNotActive = parent.querySelector('.song-vote__icon--minus');
+    voteActive.classList.toggle("song-vote__icon--hide");
+    voteNotActive.classList.toggle("song-vote__icon--hide");
+}
 
+const allItemsVote = document.querySelector("body");
+if (document.querySelector('body')) {
+    allItemsVote.addEventListener("click", (e) => {
+        const targetStart = e.target.closest(".song-vote__icon");
+        if (targetStart) {
+            const parent = targetStart.closest(".song-item");
+            voteToggle(parent);
+        }
+    });
+};
+let modalBtn = document.querySelector('.modal-live-bg--show');
+let modalIcon = document.querySelector('.modal-live__icon--active');
+let modalIconActive = document.querySelector('.modal-live__icon--hidden');
+if (document.querySelector('body')) {
+    modalBtn.addEventListener('click', function () {
+        modalIcon.classList.toggle('modal-live__icon--hide');
+        modalIconActive.classList.toggle('modal-live__icon--hide');
+    });
+};
+const inputSwitch = document.querySelector('.form-check-input');
+const bodyTheme = document.querySelector('body')
+inputSwitch.addEventListener("click", function () {
+    bodyTheme.classList.toggle('dark-theme')
+});
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.mobile-nav');
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+const form = document.querySelector(".login-form");
+if (form) {
+    function togglePasswordType(input, button) {
+        const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+        input.setAttribute('type', type);
+        button.classList.toggle('input-group-icon--active');
+    }
+
+    form.addEventListener('click', function ({target}) {
+        if (target.classList.contains('form-password-btn')) {
+            const container = target.closest('.form-password')
+            const input = container.querySelector('.form-password-input')
+            togglePasswordType(input, target)
+        }
+    });
+}
 $(".simple-select").select2({
     Search: false,
     templateResult: formatState,
     templateSelection: formatState
 });
 
-function formatState (opt) {
+function formatState(opt) {
     if (!opt.id) {
         return opt.text.toUpperCase();
     }
 
     var optimage = $(opt.element).attr('data-image');
-    if(!optimage){
+    if (!optimage) {
         return opt.text.toUpperCase();
     } else {
         var $opt = $(
